f5eff30c1adc42a97d9bd4770ba46e8787622daa
leandro <leandrochagask@gmail.com>
2021-11-11 16:47:52 -0300

    Adiciona requisito 01 - rotas

---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Search from './Search';
+
+class Login extends React.Component{
+  render() {
+    return (
+      <div data-testid="page-login">
+          <div>Página de login</div>
+
+      </div>
+    )
+  }
+}
+
+export default Login;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component{
+  render() {
+    return (
+      <div data-testid="page-profile-edit">profile-edit page</div>
+
+    )
+  }
+}
+
+export default ProfileEdit;
