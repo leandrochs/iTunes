67bb77027ab0eaa5c42e99675c43e4866b21bb11
leandro <leandrochagask@gmail.com>
2021-11-12 11:53:16 -0300

    Adiciona requisito 5 - formulario para pesquisar artistas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import Header from '../components/Header';
+import "../css/search.css"
+const MINIMUM_SIZE = 2;
 
 class Search extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+      // loading: false,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      artistName: value,
+    });
+  }
+
+  // onClickButton = () => {
+  //   this.setState({ loading: true }, async () => {
+  //     const { artistName } = this.state;
+  //     await createUser({ name: artistName });
+  //     this.setState({ loading: 'completed' });
+  //   });
+  // }
+
   render() {
+
+    const { artistName } = this.state;
+
     return (
-      <div data-testid="page-search">
+      <div className="searchPage-container" data-testid="page-search">
         <Header />
-        Search page
+        <div className="searchInput-container">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Banda ou artista"
+            onChange={ this.onInputChange }
+            value={ artistName }
+          />
+          <button 
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ (artistName.length < MINIMUM_SIZE) }
+            // onClick={ this.onClickButton }
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
